10ebb004b64ae258b6bd2e6ec96e3e0943c89495
Temile <tdanylaite@gmail.com>
2024-09-26 21:32:03 +0300

    Papildoma programa taip, kad butu galima duomenis ne tik ivesti bet ir nuskaityti is failo.Sukurtas failas kursiokai.txt bei taip pat sukurtas FailoNuskaitymas.cpp ir pamodifikuoti Stud.h, Mylib.h, Studentu_duomenys.cpp

---
+++ Mylib.h
@@ -0,0 +1,37 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <fstream>
+
+
+
+
+
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::string;
+using std::vector;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::fstream;
+
+
+
+
+#endif // MYLIB_H_INCLUDED
+
+
